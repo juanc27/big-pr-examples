d1d5d5423cf17b9380fa1aee3c099addd6822a6b
Juan Munoz <juanc.munozv@gmail.com>
2020-10-29 11:25:00 -0700

    New Standings View - Big Spaghetti PR

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,32 @@
 class Team < ActiveRecord::Base
   has_many :home_games, class_name: 'Game', foreign_key: 'home_team_id', inverse_of: 'home_team'
   has_many :away_games, class_name: 'Game', foreign_key: 'away_team_id', inverse_of: 'away_team'
+
+  def home_wins
+    home_games.where('home_score > away_score').count
+  end
+
+  def away_wins
+    away_games.where('away_score > home_score').count
+  end
+
+  def home_losses
+    home_games.where('home_score < away_score').count
+  end
+
+  def away_losses
+    away_games.where('away_score < home_score').count
+  end
+
+  def wins
+    home_wins + away_wins
+  end
+
+  def losses
+    home_losses + away_losses
+  end
+
+  def wining_pct
+    1.0 * wins / (wins + losses)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+team1 = Team.create(name: 'Golden State Warriors')
+team2 = Team.create(name: 'Los Angeles Lakers')
+team3 = Team.create(name: 'Los Angeles Clippers')
+team4 = Team.create(name: 'Phoenix Suns')
+team5 = Team.create(name: 'Sacramento Kings')
+
+first_date = DateTime.parse('2020-10-01 19:00:00 UTC')
+
+Game.create(home_team: team1, away_team: team2, home_score: 99, away_score: 88, date: first_date + 1)
+Game.create(home_team: team4, away_team: team3, home_score: 89, away_score: 92, date: first_date + 1)
+
+Game.create(home_team: team1, away_team: team4, home_score: 115, away_score: 90, date: first_date + 2)
+Game.create(home_team: team5, away_team: team3, home_score: 85, away_score: 99, date: first_date + 2)
+
+Game.create(home_team: team2, away_team: team4, home_score: 116, away_score: 91, date: first_date + 3)
+Game.create(home_team: team5, away_team: team1, home_score: 91, away_score: 100, date: first_date + 3)
+
+Game.create(home_team: team3, away_team: team1, home_score: 85, away_score: 95, date: first_date + 4)
+Game.create(home_team: team2, away_team: team5, home_score: 115, away_score: 80, date: first_date + 4)
+
+Game.create(home_team: team3, away_team: team2, home_score: 91, away_score: 100, date: first_date + 5)
+Game.create(home_team: team4, away_team: team5, home_score: 105, away_score: 100, date: first_date + 5)
+
 
 
 
